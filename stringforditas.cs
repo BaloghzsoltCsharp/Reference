0a11aee95909f94399423c58eab5fad0a202b1c4
BaloghzsoltCsharp <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2022-03-14 01:15:35 +0100

    Create stringforditas.cs

---
+++ stringforditas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace string_fordítás
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ide jöhet a string: ");
+            string bekert = Console.ReadLine();
+            Console.WriteLine();
+            bool i = true;
+            int k = bekert.Length;
+            k--;
+            //K változó = a bekért string hossza
+
+            string forditva="";
+            // a fordított stringet fogom ebbe tárolni
+
+            while (i==true)
+            {
+                forditva += bekert[k];
+                k--;
+                if (k < 0)
+                    i = false;
+            }
+            Console.WriteLine("A mondat amit beküldtél:"+bekert+"\n\n");
+            Console.WriteLine("Fordítva: "+forditva);
+            Console.ReadLine();
+
+
+        }
+    }
+}
